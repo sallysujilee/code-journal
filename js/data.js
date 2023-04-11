9120c2b619e19b9510b15be722e4daeb2604e0be
Sally Lee <sallysujilee@gmail.com>
2023-04-11 21:13:35 +0000

    Fix all errors. 1

--- js/data.js
+++ js/data.js
@@ -6,3 +6,11 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function () {
+  localStorage.setItem('data', JSON.stringify(data));
+});
+
+if (localStorage.getItem('data')) {
+  data = JSON.parse(localStorage.getItem('data'));
+}
